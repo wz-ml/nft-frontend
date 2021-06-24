0424bbc2c9ac376a15721a5ae71d6ddd010dff7d
e seng <eth.sengsavang@gmail.com>
2021-06-24 11:04:54 -0600

    Add list items under every section in sidebar

--- src/components/Marketplace/Sidebar/Sidebar.jsx
+++ src/components/Marketplace/Sidebar/Sidebar.jsx
@@ -14,15 +14,31 @@ const Sidebar = () => {
     <section className="sidebar">
       <ul className="sidebar-item" value="Status">
         <h3 className="sidebar-listheader" onClick={updateSidebar}>Status</h3>
+        <div className="list-items-container">
+          <li className="list-item">itema1</li>
+          <li className="list-item">itema2</li>
+        </div>
       </ul>
       <ul className="sidebar-item" value="Price">
         <h3 className="sidebar-listheader" onClick={updateSidebar}>Price</h3>
+        <div className="list-items-container">
+          <li className="list-item">itemb1</li>
+          <li className="list-item">itemb2</li>
+        </div>
       </ul>
       <ul className="sidebar-item" value="Categories">
         <h3 className="sidebar-listheader" onClick={updateSidebar}>Categories</h3>
+        <div className="list-items-container">
+          <li className="list-item">itemc1</li>
+          <li className="list-item">itemc2</li>
+        </div>
       </ul>
       <ul className="sidebar-item" value="Charities">
         <h3 className="sidebar-listheader" onClick={updateSidebar}>Charities</h3>
+        <div className="list-items-container">
+          <li className="list-item">itemd1</li>
+          <li className="list-item">itemd2</li>
+        </div>
       </ul>
       <div className="sidebar-item social-media-container"></div>
     </section>
