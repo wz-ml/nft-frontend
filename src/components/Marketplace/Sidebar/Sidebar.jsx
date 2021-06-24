6defc95fbf3007f6ab47d2a7ec5b2511933d7393
e seng <eth.sengsavang@gmail.com>
2021-06-24 11:41:36 -0600

    Add values to the sidebar options

--- src/components/Marketplace/Sidebar/Sidebar.jsx
+++ src/components/Marketplace/Sidebar/Sidebar.jsx
@@ -12,35 +12,35 @@ const Sidebar = () => {
 
   return (
     <section className="sidebar">
-      <ul className="sidebar-item active" value="Status">
+      <ul className="sidebar-item active" value={"Status"}>
         <h3 className="sidebar-listheader" onClick={updateSidebar}>Status</h3>
         <div className="list-items-container">
           <li className="list-item">itema1</li>
           <li className="list-item">itema2</li>
         </div>
       </ul>
-      <ul className="sidebar-item" value="Price">
+      <ul className="sidebar-item" value={"Price"}>
         <h3 className="sidebar-listheader" onClick={updateSidebar}>Price</h3>
         <div className="list-items-container">
           <li className="list-item">itemb1</li>
           <li className="list-item">itemb2</li>
         </div>
       </ul>
-      <ul className="sidebar-item" value="Categories">
+      <ul className="sidebar-item" value={"Categories"}>
         <h3 className="sidebar-listheader" onClick={updateSidebar}>Categories</h3>
         <div className="list-items-container">
           <li className="list-item">itemc1</li>
           <li className="list-item">itemc2</li>
         </div>
       </ul>
-      <ul className="sidebar-item" value="Charities">
+      <ul className="sidebar-item" value={"Charities"}>
         <h3 className="sidebar-listheader" onClick={updateSidebar}>Charities</h3>
         <div className="list-items-container">
           <li className="list-item">itemd1</li>
           <li className="list-item">itemd2</li>
         </div>
       </ul>
-      <div className="sidebar-item social-media-container"></div>
+      <div className="sidebar-item social-media-container" value={"socials"}></div>
     </section>
   )
 }
