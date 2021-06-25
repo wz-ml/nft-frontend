2fa3d3d0f1585f08ee184806db99de86e63673e1
e seng <eth.sengsavang@gmail.com>
2021-06-25 11:09:03 -0600

    Adjust html and css to separate categories and socials

--- src/components/Marketplace/Sidebar/Sidebar.jsx
+++ src/components/Marketplace/Sidebar/Sidebar.jsx
@@ -29,34 +29,36 @@ const Sidebar = () => {
 
   return (
     <section className="sidebar">
-      <ul className="sidebar-item active" value={"Status"}>
-        <h3 className="sidebar-listheader" onClick={updateSidebar}>Status</h3>
-        <div className="list-items-container">
-          <li className="list-item">Buy Now</li>
-          <li className="list-item">itema2</li>
-        </div>
-      </ul>
-      <ul className="sidebar-item" value={"Price"}>
-        <h3 className="sidebar-listheader" onClick={updateSidebar}>Price</h3>
-        <div className="list-items-container">
-          <li className="list-item">itemb1</li>
-          <li className="list-item">itemb2</li>
-        </div>
-      </ul>
-      <ul className="sidebar-item" value={"Categories"}>
-        <h3 className="sidebar-listheader" onClick={updateSidebar}>Categories</h3>
-        <div className="list-items-container">
-          <li className="list-item">itemc1</li>
-          <li className="list-item">itemc2</li>
-        </div>
-      </ul>
-      <ul className="sidebar-item" value={"Charities"}>
-        <h3 className="sidebar-listheader" onClick={updateSidebar}>Charities</h3>
-        <div className="list-items-container">
-          <li className="list-item">itemd1</li>
-          <li className="list-item">itemd2</li>
-        </div>
-      </ul>
+      <div className="sidebar-item-contrainer">
+        <ul className="sidebar-item active" value={"Status"}>
+          <h3 className="sidebar-listheader" onClick={updateSidebar}>Status</h3>
+          <div className="list-items-container">
+            <li className="list-item">Buy Now</li>
+            <li className="list-item">itema2</li>
+          </div>
+        </ul>
+        <ul className="sidebar-item" value={"Price"}>
+          <h3 className="sidebar-listheader" onClick={updateSidebar}>Price</h3>
+          <div className="list-items-container">
+            <li className="list-item">itemb1</li>
+            <li className="list-item">itemb2</li>
+          </div>
+        </ul>
+        <ul className="sidebar-item" value={"Categories"}>
+          <h3 className="sidebar-listheader" onClick={updateSidebar}>Categories</h3>
+          <div className="list-items-container">
+            <li className="list-item">itemc1</li>
+            <li className="list-item">itemc2</li>
+          </div>
+        </ul>
+        <ul className="sidebar-item" value={"Charities"}>
+          <h3 className="sidebar-listheader" onClick={updateSidebar}>Charities</h3>
+          <div className="list-items-container">
+            <li className="list-item">itemd1</li>
+            <li className="list-item">itemd2</li>
+          </div>
+        </ul>
+      </div>
       <div className="sidebar-item social-media-container" value={"socials"}>
         <Reddit />
       </div>
