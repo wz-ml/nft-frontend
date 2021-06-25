4dd008274fde1b569a0b95658e6ca72d2dfb1d24
e seng <eth.sengsavang@gmail.com>
2021-06-25 14:23:51 -0600

    Updates

---
+++ precache-manifest.bb79f90206fb76d4dcd01b2ed1bfd52f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "507eb040ffd16e4eebee",
+    "url": "./public/static/js/runtime~main.4ba81877.js"
+  },
+  {
+    "revision": "f084c6061210699fe175",
+    "url": "./public/static/js/main.2d428e93.chunk.js"
+  },
+  {
+    "revision": "39649ddf4b13b16d42a7",
+    "url": "./public/static/js/3.18734987.chunk.js"
+  },
+  {
+    "revision": "ca8dadc3c246f304f66b",
+    "url": "./public/static/js/2.4b9093b5.chunk.js"
+  },
+  {
+    "revision": "f084c6061210699fe175",
+    "url": "./public/static/css/main.6ea94f55.chunk.css"
+  },
+  {
+    "revision": "15eea219458197c7c45be4ce779758cd",
+    "url": "./public/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./public/precache-manifest.a3d2fcc775122bef2b5ef5a494e56621.js"
+  "./public/precache-manifest.bb79f90206fb76d4dcd01b2ed1bfd52f.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./public/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
