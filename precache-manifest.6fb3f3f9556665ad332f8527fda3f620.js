99aea9b9c98ebd02c126aac0839d61620c7ead17
e seng <eth.sengsavang@gmail.com>
2021-06-28 14:29:27 -0600

    Updates

---
+++ precache-manifest.6fb3f3f9556665ad332f8527fda3f620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b9be3243ae08f7e08a5ae3e5bb8d5c5",
+    "url": "./public/static/media/bcharity_logo.3b9be324.png"
+  },
+  {
+    "revision": "df78b8f314058dcfb1c6",
+    "url": "./public/static/js/runtime~main.ca696b75.js"
+  },
+  {
+    "revision": "3f75fc5c709ee3bf18f1",
+    "url": "./public/static/js/main.c54b32b5.chunk.js"
+  },
+  {
+    "revision": "c9e9b8523cad449af5c4",
+    "url": "./public/static/js/3.a9e9a2b8.chunk.js"
+  },
+  {
+    "revision": "b7ca5e555382949d8181",
+    "url": "./public/static/js/2.bb45f206.chunk.js"
+  },
+  {
+    "revision": "3f75fc5c709ee3bf18f1",
+    "url": "./public/static/css/main.3301f9a7.chunk.css"
+  },
+  {
+    "revision": "5705a51996463ddec1549347c04c63cf",
+    "url": "./public/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./public/precache-manifest.afdb3695322cb001fae0c815f70265a8.js"
+  "./public/precache-manifest.6fb3f3f9556665ad332f8527fda3f620.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./public/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
