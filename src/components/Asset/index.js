7b18b46522946c50ed7406b1876cc6ca19aa623f
e seng <eth.sengsavang@gmail.com>
2021-06-28 13:48:48 -0600

    Add comments to the code

--- src/components/Asset/index.js
+++ src/components/Asset/index.js
@@ -15,10 +15,23 @@ const Asset = () => {
   const [tokenCollection, setTokenCollection] = useState("");
   const [imgUrl, setImgUrl] = useState("");
 
+  /**
+   * Uses React effects perform one-time actions.
+   *
+   * - Adds a load event listener to fetch the details of the connected NFT
+   */
   useEffect(() => {
     window.addEventListener("load", getDetails);
   });
 
+  /**
+   * Gets the details of the connected NFT, found within the url.
+   * A valid NFT collection address and tokenID are expected within
+   * the url in the format:
+   * <code>https://(URL)/asset/(Collection Address)/(tokenID)</code>
+   *
+   * Will fetch the data needed from the opensea API and update the page.
+   */
   async function getDetails(){
     let urlParts = window.location.pathname.split('/');
     const [collectionAddr, tokenID] = urlParts.splice(-2);
@@ -29,6 +42,12 @@ const Asset = () => {
       .catch((err) => console.error(err));
   }
 
+  /**
+   * Updates the page with the new token details
+   *
+   * @param tokenData {Object} the parsed JSON object retrieved after fetching
+   * details from the opensea API.
+   */
   async function updateDetails(tokenData){
     setTokenName(tokenData.name)
     setTokenCollection(tokenData.collection.name);
