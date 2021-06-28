7fd7f2640e261bc45698f0487c62dde1a2c91e60
e seng <eth.sengsavang@gmail.com>
2021-06-28 11:09:56 -0600

    Add function to read current url to request asset info

--- src/components/Asset/index.js
+++ src/components/Asset/index.js
@@ -4,9 +4,24 @@
  * @version 2021.06.28 - Base development
  * @since 2021.06.28
  */
+import React from "react";
+import fetch from "node-fetch";
 
 const Asset = () => {
+  const API_URL = "https://rinkeby-api.opensea.io/api/v1";
 
+  async function getDetails(){
+    let urlParts = window.location.pathname.split('/');
+    const [collectionAddr, tokenID] = urlParts.splice(-2);
+
+    fetch(`${API_URL}/asset/${collectionAddr}/${tokenID}`, {method: "GET"})
+      .then((res) => response = res.json)
+      .catch((err) => console.error(err));
+  }
+
+  return(
+    <h2 onClick={getDetails}>Asset page</h2>
+  );
 }
 
 export default Asset;
