7a24d95b2c12df86b69e596c1ee6d4494d28b55e
e seng <eth.sengsavang@gmail.com>
2021-06-28 11:16:59 -0600

    Create function to collect asset info

--- src/components/Asset/index.js
+++ src/components/Asset/index.js
@@ -5,18 +5,26 @@
  * @since 2021.06.28
  */
 import React from "react";
+import {useState} from "react";
 import fetch from "node-fetch";
 
 const Asset = () => {
   const API_URL = "https://rinkeby-api.opensea.io/api/v1";
+  const [imgUrl, setImgUrl] = useState("");
 
   async function getDetails(){
     let urlParts = window.location.pathname.split('/');
     const [collectionAddr, tokenID] = urlParts.splice(-2);
 
     fetch(`${API_URL}/asset/${collectionAddr}/${tokenID}`, {method: "GET"})
-      .then((res) => response = res.json)
+      .then((res) => res.json())
+      .then((json) => updateDetails(json))
       .catch((err) => console.error(err));
+  }
+
+  async function updateDetails(tokenData){
+    setImgUrl(tokenData.image_url);
+    console.log(tokenData);
   }
 
   return(
