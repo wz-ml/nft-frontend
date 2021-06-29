66594dc46265e579561499566b5914a25e707e41
e seng <eth.sengsavang@gmail.com>
2021-06-29 15:28:32 -0600

    Remove styled components within marketplace page

--- src/components/Marketplace/index.js
+++ src/components/Marketplace/index.js
@@ -37,10 +37,7 @@ export default class Marketplace extends React.Component {
   render() {
     return (
       <div>
-        <a href={GITHUB_URL}>
-          <ImgRibbon src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub" />
-        </a>
-        <Header>
+        <header>
           <h1 className="title">
             The Ship's Log
           </h1>
@@ -52,7 +49,7 @@ export default class Marketplace extends React.Component {
               <img alt="OpenSea logo" className="mr-2" src="/opensea-logo.png" />OpenSea.js
             </a> example dapp
           </h6>
-        </Header>
+        </header>
         <Sidebar />
         <main>
           <Log
@@ -64,25 +61,3 @@ export default class Marketplace extends React.Component {
   }
 }
 
-const Header = styled.header`
-  border-bottom: 1px solid lightgrey;
-  padding: 10px;
-  text-align: center;
-  background-color: #f4f9fd;
-
-  h6 img {
-    width: 24px;
-  }
-`
-
-const ImgRibbon = styled.img`
-  width: 150px;
-  position: absolute;
-  top: 0;
-  right: 0;
-  border: 0;
-
-  @media(max-width: 600px) {
-    width: 80px;
-  }
-`
