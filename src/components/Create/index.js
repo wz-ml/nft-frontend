9490f321fc0c8ee376673f729eaf5df26aeed9e9
Eric Li <summer.eric1@gmail.com>
2021-06-29 17:07:25 -0600

    added changes to index.js for the create page

--- src/components/Create/index.js
+++ src/components/Create/index.js
@@ -3,28 +3,40 @@ import React from "react";
 
 const Create = () => {
     return (
+        <div className = "createThing">
+ 
+        <h1>Create new item</h1>
 
-        <div class = "createThing">
-        {/* <img src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Kevin_Durant_%28Wizards_v._Warriors%2C_1-24-2019%29_%28cropped%29.jpg" id="wallet-img" alt="Your very own NFT Wallet" class="wallet-img">
-        </img>
-
-        <br></br><a href="#">CREATE NEW ITEM HERE</a> */}
-        <h1>HEADER 1</h1>
+        <strong>Image, Video, Audio, or 3D Model</strong> <br></br>
+        File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG, GLB, GLTF. Max size: 40 MB
 
         <form>
+        <br></br><br></br>
         <strong>Name *</strong> <br></br>
         <input type="text"></input>
+        <br></br><br></br>
+        <strong>External Link</strong> 
+        <br></br>
+        OpenSea will include a link to this URL on this item's detail page, so that users can click to learn more about it. <br></br>You are welcome to link to your own webpage with more details.
 
-        <br></br><strong>External Link</strong> 
-        <p>OpenSea will include a link to this URL on this item's detail page, so that users can click to learn more about it. You are welcome to link to your own webpage with more details.
-</p>
-<input type="text"></input>
+        <br></br><input type="text"></input>
 
 
-    <br></br><strong>Description</strong>
-    <p>The description will be included on the item's detail page underneath its image. Markdown syntax is supported.</p>
-    <input type="text"></input>
+    <br></br><br></br><strong>Description</strong>
+    <br></br>The description will be included on the item's detail page underneath its image.
+    <span style= {{color:"blue" }} > Markdown </span>
+   syntax is supported.
 
+    {/* const element = <h1 style= {{ color:'red' }} > Hello world</h1> */}
+    <br></br><input type="text"></input>
+
+    <br></br><br></br><strong>Supply</strong>
+    <br></br>The number of copies that can be minted. No gas cost to you! Quantities above one coming soon.
+    <br></br><input type="text"></input>
+
+    <br></br><br></br><strong>Freeze Metadata</strong>
+    <br></br>Freezing your metadata will allow you to permanently lock and store all of this item's content in decentralized file storage.
+    <br></br><input type="text"></input>
 
 
         </form>
