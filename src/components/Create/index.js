4c17d23c7d5134056fe686c19cbca9b004f4c2a8
Eric Li <summer.eric1@gmail.com>
2021-06-29 11:11:47 -0600

    updated index.js and AssetMetadata.css

--- src/components/Create/index.js
+++ src/components/Create/index.js
@@ -5,8 +5,30 @@ const Create = () => {
     return (
 
         <div class = "createThing">
-        {/* <a href="#">CREATE NEW ITEM HERE</a>
-        <h1>HEADER 1</h1> */}
+        {/* <img src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Kevin_Durant_%28Wizards_v._Warriors%2C_1-24-2019%29_%28cropped%29.jpg" id="wallet-img" alt="Your very own NFT Wallet" class="wallet-img">
+        </img>
+
+        <br></br><a href="#">CREATE NEW ITEM HERE</a> */}
+        <h1>HEADER 1</h1>
+
+        <form>
+        <strong>Name *</strong> <br></br>
+        <input type="text"></input>
+
+        <br></br><strong>External Link</strong> 
+        <p>OpenSea will include a link to this URL on this item's detail page, so that users can click to learn more about it. You are welcome to link to your own webpage with more details.
+</p>
+<input type="text"></input>
+
+
+    <br></br><strong>Description</strong>
+    <p>The description will be included on the item's detail page underneath its image. Markdown syntax is supported.</p>
+    <input type="text"></input>
+
+
+
+        </form>
+
         </div>
 
 
