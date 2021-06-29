947e20f6ff8d4f6f96ea7d2c926320e1b529edf8
Eric Li <summer.eric1@gmail.com>
2021-06-28 20:23:00 -0600

    index.js for Create

---
+++ src/components/Create/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Create = () => {
+    return (
+
+        <div class = "createThing">
+        {/* <a href="#">CREATE NEW ITEM HERE</a>
+        <h1>HEADER 1</h1> */}
+        </div>
+
+
+    )
+};
+
+
+export default Create;
