964043e56128bfc8f6b3867f2e9ce7d9c3161fc7
Eric Li <summer.eric1@gmail.com>
2021-06-30 16:53:58 -0600

    added onto the css file for Create, so now it looks really similar to the testnet website, including the position, the bold text, some coloured text, and textbox positions and their sizes

--- src/components/Create/index.js
+++ src/components/Create/index.js
@@ -1,42 +1,42 @@
 import React from "react";
-
+import "./index.css";
 
 const Create = () => {
     return (
         <div className = "createThing">
  
-        <h1>Create new item</h1>
+        <h1 className = "head">Create new item</h1>
 
-        <strong>Image, Video, Audio, or 3D Model</strong> <br></br>
+        <h4 className = "file">
+        <strong className = "file_descrip">Image, Video, Audio, or 3D Model</strong> <br></br>
+        <p className="file_types">
         File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG, GLB, GLTF. Max size: 40 MB
+        </p>
+        </h4>
 
         <form>
         <br></br><br></br>
-        <strong>Name *</strong> <br></br>
-        <input type="text"></input>
-        <br></br><br></br>
-        <strong>External Link</strong> 
+        
+        <h4 className = "name">
+        <strong className = "name_text">Name *</strong> <br></br>
+        <input className = "name_textbox" type="text"></input>
+        </h4>
+
         <br></br>
-        OpenSea will include a link to this URL on this item's detail page, so that users can click to learn more about it. <br></br>You are welcome to link to your own webpage with more details.
-
-        <br></br><input type="text"></input>
-
-
-    <br></br><br></br><strong>Description</strong>
-    <br></br>The description will be included on the item's detail page underneath its image.
+        
+    <h4 className="description">
+    <strong className="description_title">Description</strong>
+    <p className="description_text">
+    The description will be included on the item's detail page underneath its image.
     <span style= {{color:"blue" }} > Markdown </span>
-   syntax is supported.
+    syntax is supported.
+    </p>
+    <textarea className="description_textbox" name="comment[body]" rows="1" cols="50" wrap="physical" id="comment_text_area"></textarea>
+    </h4>
 
-    {/* const element = <h1 style= {{ color:'red' }} > Hello world</h1> */}
-    <br></br><input type="text"></input>
+    
 
-    <br></br><br></br><strong>Supply</strong>
-    <br></br>The number of copies that can be minted. No gas cost to you! Quantities above one coming soon.
-    <br></br><input type="text"></input>
-
-    <br></br><br></br><strong>Freeze Metadata</strong>
-    <br></br>Freezing your metadata will allow you to permanently lock and store all of this item's content in decentralized file storage.
-    <br></br><input type="text"></input>
+   
 
 
         </form>
