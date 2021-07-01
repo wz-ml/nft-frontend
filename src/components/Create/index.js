f564eb170fad43d48f833dc1dffd8a0ec7c5d008
Eric Li <summer.eric1@gmail.com>
2021-07-01 13:57:28 -0600

    Changed the position of the text and textboxes such that it remains consistent regardless of different display, and also added in grey preview text in the textboxes, however still need to adjust the font and size of the grey preview text

--- src/components/Create/index.js
+++ src/components/Create/index.js
@@ -5,34 +5,59 @@ const Create = () => {
     return (
         <div className = "createThing">
  
-        <h1 className = "head">Create new item</h1>
+        <div className = "createNewItem_text">
+            <h1>
+            Create new item
+            </h1>
+        </div>
+        
 
-        <h4 className = "file">
-        <strong className = "file_descrip">Image, Video, Audio, or 3D Model</strong> <br></br>
+        <div className="file_types">
+        {/* <h4> */}
+        <strong className = "file_descrip">Image, Video, Audio, or 3D Model</strong>
+        <br></br>
+        File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG, GLB, GLTF. Max size: 40 MB
+        {/* </h4> */}
+        </div>
+
+
+        {/* <h4 className = "file">
+        <strong className = "file_descrip">Image, Video, Audio, or 3D Model</strong> 
+        </h4>
+        
         <p className="file_types">
         File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG, GLB, GLTF. Max size: 40 MB
-        </p>
-        </h4>
+        </p> */}
+
+
 
         <form>
         <br></br><br></br>
-        
-        <h4 className = "name">
-        <strong className = "name_text">Name *</strong> <br></br>
-        <input className = "name_textbox" type="text"></input>
-        </h4>
+        <div className = "name">
+        {/* <div className = "name_text"> */}
+        <strong>Name *</strong> <br></br>
+        {/* </div> */}
+
+        {/* <div className = "name_textbox"> */}
+        <input className="name_textbox_size" type="text" placeholder="Item Name"></input>
+        {/* </div> */}
+        </div>
 
         <br></br>
         
-    <h4 className="description">
-    <strong className="description_title">Description</strong>
-    <p className="description_text">
-    The description will be included on the item's detail page underneath its image.
-    <span style= {{color:"blue" }} > Markdown </span>
-    syntax is supported.
-    </p>
-    <textarea className="description_textbox" name="comment[body]" rows="1" cols="50" wrap="physical" id="comment_text_area"></textarea>
-    </h4>
+        {/* <h4 className = "description"> */}
+        <div className = "description">
+        <strong>Description</strong>
+
+        <p className="description_text">
+        The description will be included on the item's detail page underneath its image.
+        <span style= {{color:"blue" }} > Markdown </span>
+        syntax is supported.
+        </p>
+        
+        <textarea className="description_textbox" name="comment[body]" rows="1" cols="50" wrap="physical" id="comment_text_area" placeholder="Provide a detailed description of your item."></textarea>
+        </div>
+        {/* </h4> */}
 
     
 
