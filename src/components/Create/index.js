aeec23faaa2e8f77e0f36a7aabc6c71cb5399acd
stalkeraturdoor <tonyshi12@gmail.com>
2021-07-02 11:01:05 -0600

    added getFormData()

--- src/components/Create/index.js
+++ src/components/Create/index.js
@@ -13,17 +13,17 @@ const Create = () => {
 
         <form>
         <strong>Name *</strong> <br></br>
-        <input type="text"></input>
+        <input type="text" id="_name"></input>
 
         <br></br><strong>External Link</strong> 
         <p>OpenSea will include a link to this URL on this item's detail page, so that users can click to learn more about it. You are welcome to link to your own webpage with more details.
-</p>
-<input type="text"></input>
+        </p>
+        <input type="text" id="_external_url"></input>
 
 
     <br></br><strong>Description</strong>
     <p>The description will be included on the item's detail page underneath its image. Markdown syntax is supported.</p>
-    <input type="text"></input>
+    <input type="text" id="_description"></input>
 
 
 
@@ -32,7 +32,23 @@ const Create = () => {
         </div>
 
 
+
     )
+
+     function getFormData(){
+
+        let formData = {
+
+            "name": String(document.getElementById("_name")),
+            "external_url": String(document.getElementById("_external_url")),
+            "description": String(document.getElementById("_description"))
+
+        }
+
+        return formData;
+
+     }
+
 };
 
 
