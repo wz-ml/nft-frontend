6d9054812dde897b5c9a53ecc679c9a4d69b7195
Eric Li <summer.eric1@gmail.com>
2021-07-07 14:30:04 -0600

    Fixed up the marketplace, also recorded what changes were made to fix it

--- src/components/common/assetInfo/AssetMetadata.js
+++ src/components/common/assetInfo/AssetMetadata.js
@@ -2,6 +2,20 @@ import React from 'react'
 import "./AssetMetadata.css";
 
 export default class AssetMetadata extends React.Component {
+  
+  scalePhoto(event){
+    let height = event.target.height;
+    let width = event.target.width;
+
+    console.log(`${height} x ${width}`);
+
+    if(height < 100 && width < 100){
+      event.target.classList.add("tiny-img");
+    }
+
+    event.target.classList.add("card-image");
+  }
+
   render() {
     const { asset } = this.props
 
@@ -27,10 +41,10 @@ export default class AssetMetadata extends React.Component {
       <React.Fragment>
         <div className="card-container">
           <div className="img-container">
-            <a target="_blank" rel="noopener noreferrer" className="text-center d-inline-block m-100" href={`/asset/${assetAddr}/${assetId}`}>
+            <a target="_blank" rel="noopener noreferrer" className="image-link text-center d-inline-block m-100" href={`/asset/${assetAddr}/${assetId}`}>
               <img
-                className="card-image"
                 alt="Asset artwork"
+                onLoad={this.scalePhoto}
                 src={assetImage} />
             </a>
           </div>
@@ -38,7 +52,8 @@ export default class AssetMetadata extends React.Component {
           <div className="card-body h-25">
             <div className="card-text text-truncate">
               <a target="_blank" rel="noopener noreferrer" href={`/asset/${assetAddr}/${assetId}`} className="card-link">
-                <h5>{asset.name} {assetContractName}</h5>
+                <h4>{asset.name}</h4>
+                <h5><i>{assetContractName}</i></h5>
               </a>
             </div>
           </div>
