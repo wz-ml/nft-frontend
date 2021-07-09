6d50003c9efe32477f493ad154444de945e7e629
e seng <eth.sengsavang@gmail.com>
2021-07-09 14:17:45 -0600

    Updates

---
+++ precache-manifest.e686031f302dd11147d37aaca4cf182c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f3c8d9b3f1cfee900f9",
+    "url": "https://BCharity-Net.github.io/static/css/main.88f5e3bd.chunk.css"
+  },
+  {
+    "revision": "6f3c8d9b3f1cfee900f9",
+    "url": "https://BCharity-Net.github.io/static/js/main.702e79b9.chunk.js"
+  },
+  {
+    "revision": "9d63b252ff851a074b83",
+    "url": "https://BCharity-Net.github.io/static/js/runtime~main.d9faeeef.js"
+  },
+  {
+    "revision": "f944a99b735bf4bcc194",
+    "url": "https://BCharity-Net.github.io/static/js/2.8c17ab08.chunk.js"
+  },
+  {
+    "revision": "d0d585d26e9836b69362",
+    "url": "https://BCharity-Net.github.io/static/js/3.09af949c.chunk.js"
+  },
+  {
+    "revision": "3b9be3243ae08f7e08a5ae3e5bb8d5c5",
+    "url": "https://BCharity-Net.github.io/static/media/bcharity_logo.3b9be324.png"
+  },
+  {
+    "revision": "2008f59667029b679afe78a9889e8013",
+    "url": "https://BCharity-Net.github.io/static/media/cat.2008f596.jpg"
+  },
+  {
+    "revision": "cf24d7f7347f2a82f05a2b26cb9fae47",
+    "url": "https://BCharity-Net.github.io/static/media/metamask_icon.cf24d7f7.png"
+  },
+  {
+    "revision": "274cedb353fc57579905a7436b16a4db",
+    "url": "https://BCharity-Net.github.io/static/media/collection.274cedb3.jpg"
+  },
+  {
+    "revision": "fd736309eef4a1415cc7961726a09102",
+    "url": "https://BCharity-Net.github.io/static/media/sale_icon.fd736309.png"
+  },
+  {
+    "revision": "972a70e68cd300d153e1dd5a9f77d58a",
+    "url": "https://BCharity-Net.github.io/static/media/login.972a70e6.svg"
+  },
+  {
+    "revision": "2ed966fd356345bcfa363060be504735",
+    "url": "https://BCharity-Net.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://bcharity-net.github.io/nft-marketplace/precache-manifest.22af004e7193479a5bf3f4345bab55e7.js"
+  "https://BCharity-Net.github.io/precache-manifest.e686031f302dd11147d37aaca4cf182c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://bcharity-net.github.io/nft-marketplace/index.html", {
+workbox.routing.registerNavigationRoute("https://BCharity-Net.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
