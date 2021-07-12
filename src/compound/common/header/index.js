8c57f6374dfcfda5016d181b1f409094647fa92d
lij313 <lij313@mcmaster.ca>
2021-07-12 11:27:28 -0600

    Modify the user icon size and dropdown menu position

--- src/compound/common/header/index.js
+++ src/compound/common/header/index.js
@@ -84,9 +84,7 @@ function DropdownMenu(){
     function DropdownItem(props){
         return (
             <a href="#" className="menu-item">
-                <span className="icon-button">{props.leftIcon}</span>
                 {props.children}
-                <span className="icon-right">{props.rightIcon}</span>
             </a>
         );
     }
